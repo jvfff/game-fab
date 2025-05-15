2d7af3c5680e81fd9fcead60c10e89b2ceddd022
Anderson Felipe Dias Domingos <felipedomingos.sun@gmail.com>
2025-05-15 13:09:34 -0300

    feat/Menu
    commit com o menu principal, de pause e de game over além da criação de alguns prefabs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.EditorTools;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -6,10 +7,15 @@ public class GameManager : MonoBehaviour
 
     PlayerStats status;
     GameObject player;
-    public bool menu;
+    
+    public GameObject panelMenu;
+    public GameObject panelPause;
+    public GameObject panelGameOver;
+    
 
     void Awake()
     {
+        CarregarMenu();
         player = GameObject.FindGameObjectWithTag("Player");
         if (player != null)
         {
@@ -24,11 +30,16 @@ public class GameManager : MonoBehaviour
         {
             GameOver();
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) { PauseGame(); }
     }
 
 
     private void GameOver()
     {
+        panelGameOver.SetActive(true);
+        panelPause.SetActive(false);
+        panelMenu.SetActive(false);
         Debug.Log("Você morreu");
         ResetGame();
     }
@@ -36,5 +47,45 @@ public class GameManager : MonoBehaviour
     private void ResetGame()
     {
 
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        panelPause.SetActive(true);
+    }
+
+    public void UnPauseGame()
+    {
+        Time.timeScale = 1;
+        panelPause.SetActive(false);
+    }
+
+    public void CarregarMenu()
+    {
+        Time.timeScale = 0;
+        panelMenu.SetActive(true);
+        panelPause.SetActive(false);
+        panelGameOver.SetActive(false);
+    }
+
+    public void IniciarJogo()
+    {
+        //essa função tem q carregar a cena
+        panelMenu.SetActive(false);
+        panelPause.SetActive(false);
+        panelGameOver.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void SairDoJogo()
+    {
+        Debug.Log("Saindo do jogo...");
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;  // Fecha o modo Play no Editor
+        #else
+        Application.Quit();  // Fecha o jogo compilado
+        #endif
     }
 }
